fcbcef9da9f44d110521c4a72b4d13dfe6bf213b
LeandroXavierDaSilva <126625221+LeandroXavierDaSilva@users.noreply.github.com>
2024-04-11 20:09:40 -0300

    ArvoreBinariaFuncoes.py

--- Trabalho Prático/ArvoreBinariaFuncoes.py
+++ Trabalho Prático/ArvoreBinariaFuncoes.py
@@ -1,13 +1,18 @@
+# Crie funções que receba uma referência ao nó raiz de uma árvore binária, que armazena números inteiros, e determinam:
+# 1. número de nós de forma recursiva
+# 2. maior valor armazenada na árvore de forma iterativa
+# 3. se um item N está armazenada na árvore binária de forma recursiva
+
 from Node import Node
 from Queue import Queue
 
-def nodeCount(root):
+def contagemNos(root):
     if not root:
         return 0
-    return nodeCount(root.left) + nodeCount(root.right) + 1
+    return contagemNos(root.left) + contagemNos(root.right) + 1
 
-def searchMax(root):
-    q = Queue(nodeCount(root))
+def maiorValor(root):
+    q = Queue(contagemNos(root))
     q.enqueue(root)
     max_value = root.data
     while not q.isEmpty():
@@ -21,18 +26,21 @@ def searchMax(root):
 
     return max_value
 
-def searchN(root, n):
+def procuraN(root, n):
     if not root:
         return False
     
-    return root.data == n or searchN(root.left, n) or searchN(root.right, n)
+    return root.data == n or procuraN(root.left, n) or procuraN(root.right, n)
 
-n = Node(2)
-n.left = Node(-3)
-n.right = Node(7)
-n.left.left = Node(5)
+n = Node(1)
+n.left = Node(5)
+n.right = Node(11)
+n.left.left = Node(4)
+n.left.right = Node(9)
+n.right.left = Node(21)
+n.right.right = Node(8)
 
-print(nodeCount(n))
-print(searchMax(n))
-print(searchN(n, 7))
-print(searchN(n, 0))
+print(contagemNos(n))
+print(maiorValor(n))
+print(procuraN(n, 21))
+print(procuraN(n, 7))
