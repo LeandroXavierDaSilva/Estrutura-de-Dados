53b22baa40a433901d021f365862a45441cf8518
LeandroXavierDaSilva <126625221+LeandroXavierDaSilva@users.noreply.github.com>
2024-04-11 19:23:18 -0300

    Intercalar.py
    Dada uma pilha de inteiros, reorganize os elementos em uma fila, de tal modo que os elementos da primeira metade da pilha estão intercalados com os elementos da segunda metade da pilha.

--- Trabalho Prático/Intercalar.py
+++ Trabalho Prático/Intercalar.py
@@ -1,7 +1,7 @@
 # Problema 03: Dada uma pilha de inteiros, reorganize os elementos em uma fila, de tal modo que os elementos da primeira metade da pilha estão intercalados com os elementos da segunda metade da pilha.
 
-From Stack import Stack
-From Queue import Queue
+from Stack import Stack
+from Queue import Queue
 
 def interleaver(s):
     q = Queue(s.count)
